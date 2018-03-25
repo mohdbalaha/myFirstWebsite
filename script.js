616e8eca6d1ca5f03736eae9677cfc22d104d839
Mohamad Balaha <mohdbalaha@yahoo.co.jp>
2018-03-26 01:14:18 +0900

    added two buttons

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let btn = document.getElementById('cipher');
 btn.onclick = function () {
-    out.innerText = rot13(txt.value);
+    // out.innerText = rot13(txt1.value);
+    txt.value=rot13(txt.value);
+}
+
+let btn2 = document.getElementById('decipher');
+btn2.onclick= function(){
+    txt.value=deRot13(txt.value);
 }
 
 let txt = document.getElementById('text');
 
-let out = document.getElementById('output');
+
+
+// let out = document.getElementById('output');
 
 function rot13(message) {
     //your code here
@@ -41,6 +49,56 @@ function rot13(message) {
                     char = String.fromCharCode(ascii);
                 } else {
                     ascii -= 13;
+                    char = String.fromCharCode(ascii);
+                }
+                newWord += char;
+
+            } else {
+                //Not a letter
+                char = String.fromCharCode(ascii);
+                newWord += char;
+            }
+        }
+        return newWord;
+    }
+
+    return newMessage;
+}
+
+function deRot13(message) {
+    //your code here
+    let arr = message.split(' ');
+    let newArr = [];
+    for (let item of arr) {
+        newArr.push(cipher(item));
+    }
+    let newMessage = newArr.join(' ');
+    //function that ciphers a single word
+    function cipher(word) {
+        let char = '';
+        let ascii = 0;
+        let newWord = '';
+        for (let index in word) {
+            ascii = word.charCodeAt(index);
+
+            if (ascii >= 97 && ascii <= 122) {
+                //Small letter
+                if (ascii >= 109 && ascii <= 122) {
+                    ascii -= 13;
+                    char = String.fromCharCode(ascii);
+                } else {
+                    ascii += 13;
+                    char = String.fromCharCode(ascii);
+                }
+                newWord += char;
+
+            } else if (ascii >= 65 && ascii <= 90) {
+                //Capital letter
+                if (ascii >= 77 && ascii <= 90) {
+                    ascii -= 13;
+                    char = String.fromCharCode(ascii);
+                } else {
+                    ascii += 13;
                     char = String.fromCharCode(ascii);
                 }
                 newWord += char;
