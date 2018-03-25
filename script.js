bddd9281c8cb451883b0fad0175461cc8862a3e9
Mohamad Balaha <mohdbalaha@yahoo.co.jp>
2018-03-26 01:44:59 +0900

    changed to my website

--- script.js
+++ script.js
@@ -1,116 +1,13 @@
-let btn = document.getElementById('cipher');
-btn.onclick = function () {
-    // out.innerText = rot13(txt1.value);
-    txt.value=rot13(txt.value);
-}
+window.onload = () => {
+  const submitButton = document.getElementById("submit");
+  const comment = document.getElementById("comment");
+  const names = document.getElementById("names");
 
-let btn2 = document.getElementById('decipher');
-btn2.onclick= function(){
-    txt.value=deRot13(txt.value);
-}
-
-let txt = document.getElementById('text');
-
-
-
-// let out = document.getElementById('output');
-
-function rot13(message) {
-    //your code here
-    let arr = message.split(' ');
-    let newArr = [];
-    for (let item of arr) {
-        newArr.push(cipher(item));
-    }
-    let newMessage = newArr.join(' ');
-    //function that ciphers a single word
-    function cipher(word) {
-        let char = '';
-        let ascii = 0;
-        let newWord = '';
-        for (let index in word) {
-            ascii = word.charCodeAt(index);
-
-            if (ascii >= 97 && ascii <= 122) {
-                //Small letter
-                if (ascii >= 97 && ascii <= 109) {
-                    ascii += 13;
-                    char = String.fromCharCode(ascii);
-                } else {
-                    ascii -= 13;
-                    char = String.fromCharCode(ascii);
-                }
-                newWord += char;
-
-            } else if (ascii >= 65 && ascii <= 90) {
-                //Capital letter
-                if (ascii >= 65 && ascii <= 77) {
-                    ascii += 13;
-                    char = String.fromCharCode(ascii);
-                } else {
-                    ascii -= 13;
-                    char = String.fromCharCode(ascii);
-                }
-                newWord += char;
-
-            } else {
-                //Not a letter
-                char = String.fromCharCode(ascii);
-                newWord += char;
-            }
-        }
-        return newWord;
-    }
-
-    return newMessage;
-}
-
-function deRot13(message) {
-    //your code here
-    let arr = message.split(' ');
-    let newArr = [];
-    for (let item of arr) {
-        newArr.push(cipher(item));
-    }
-    let newMessage = newArr.join(' ');
-    //function that ciphers a single word
-    function cipher(word) {
-        let char = '';
-        let ascii = 0;
-        let newWord = '';
-        for (let index in word) {
-            ascii = word.charCodeAt(index);
-
-            if (ascii >= 97 && ascii <= 122) {
-                //Small letter
-                if (ascii >= 109 && ascii <= 122) {
-                    ascii -= 13;
-                    char = String.fromCharCode(ascii);
-                } else {
-                    ascii += 13;
-                    char = String.fromCharCode(ascii);
-                }
-                newWord += char;
-
-            } else if (ascii >= 65 && ascii <= 90) {
-                //Capital letter
-                if (ascii >= 77 && ascii <= 90) {
-                    ascii -= 13;
-                    char = String.fromCharCode(ascii);
-                } else {
-                    ascii += 13;
-                    char = String.fromCharCode(ascii);
-                }
-                newWord += char;
-
-            } else {
-                //Not a letter
-                char = String.fromCharCode(ascii);
-                newWord += char;
-            }
-        }
-        return newWord;
-    }
-
-    return newMessage;
-}
+  submitButton.onclick = function () {
+    var heading = document.createElement("p");
+    var heading_text = document.createTextNode('\"' + comment.value + '\"' + ' By \"' + names.value + '\"');
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+  };
+  
+};
